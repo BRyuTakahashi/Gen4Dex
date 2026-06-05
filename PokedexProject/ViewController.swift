23f67c38b71d81c4bd627d07fd329096a07850cb
Bruno Ryu Takahashi <166444188+BRyuTakahashi@users.noreply.github.com>
2026-06-05 00:07:41 -0300

    Initial Commit

---
+++ PokedexProject/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PokedexProject
+//
+//  Created by Bruno Ryu Takahashi on 05/06/26.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
